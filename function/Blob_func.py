b614a925f633987f46ff42fd438cfa3ad31e9247
Hoku <hokuto.u0113@gmail.com>
2023-02-09 03:30:43 +0900

    send danger img

--- function/Blob_func.py
+++ function/Blob_func.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from PIL import Image
 
 # # Azure Blob config
-conn_str = "DefaultEndpointsProtocol=https;AccountName=preventer;AccountKey=uLzANa2nsp7FGZjbvMkztStgvCnT84vOC2dsmhpH/Z9BRG6M2ytosbiCtSNN2Em/GmsD6QhuGzeD+ASt3+Jn7w==;EndpointSuffix=core.windows.net"
+conn_str = ""
 container = "danger"
 
 def send_blob(frame):
@@ -15,16 +15,15 @@ def send_blob(frame):
     now = datetime.today().strftime('%Y%m%d_%H%M%S')
     file_name = f"photo_{now}.png"
     image_frame = Image.fromarray(frame)
-
-    binary = io.BytesIO()
-    image_frame.save(binary, format="png")
-    png_buffer = binary.getvalue()
+    img_byte_arr = io.BytesIO()
+    image_frame.save(img_byte_arr, format="png")
+    png_buffer = img_byte_arr.getvalue()
 
     print(type(png_buffer))
 
     # send to blob
     blob_client = service_client.get_blob_client(container=container, blob=file_name)
-    blob_client.upload_blob(png_buffer, overwrite=True, content_settings="image/png")
+    blob_client.upload_blob(png_buffer, overwrite=True)
 
 
 
