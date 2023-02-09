72036001545217047cecc3ade41b937d4216840c
Hoku113 <hokuto.u0113@gmail.com>
2023-02-10 02:26:32 +0900

    youtube video stream

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-import datetime
+import cv2
+import pafy
 
-print(datetime.datetime.now())
+# キャプチャするYouTube動画のURLを設定します
+url = "https://www.youtube.com/watch?v=ASRmcWj4gd4"
+
+video = pafy.new(url)
+best = video.getbest(preftype="mp4")
+cap = cv2.VideoCapture(best.url,)
+
+while (True):
+    ret,frame = cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(20) & 0xFF == ord('q'):
+        break    
+
+cap.release()
+cv2.destroyAllWindows()
+
+# install pafy and youtube_dl module
+
+# comment out backend_youtube_dl.py line 53, 54
