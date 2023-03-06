f0a2ca44ea7e8fad2e50ee7118d4d1377fd2afb5
Hoku <hokuto.u0113@gmail.com>
2023-03-07 00:58:34 +0900

    running as command line base

--- HumanPoseEstimation3D.py
+++ HumanPoseEstimation3D.py
@@ -3,12 +3,14 @@ import cv2
 import threading
 import json
 import pafy
+import argparse
 from openvino.runtime import Core
 from function.process_function import *
 from engine3d.draw import Plotter3d
 from function.utils import VideoPlayer
 from engine3d.thread import MultiThread
 
+# model and 3d plot settings 
 
 focal_length = -1  # default
 stride = 8
@@ -17,8 +19,6 @@ skeleton_set = None
 processing_times = []
 mean_time = 0
 fx = -1
-
-first_time = True
 
 MODEL_PATH = "./model/intel/human-pose-estimation-3d-0001/FP32/human-pose-estimation-3d-0001.xml"
 MODEL_WEIGHTS_PATH = "./model/intel/human-pose-estimation-3d-0001/FP32/human-pose-estimation-3d-0001.bin"
@@ -48,42 +48,55 @@ with open(FILE_PATH, "r") as f:
 R = np.array(extrinsics['R'], dtype=np.float32)
 t = np.array(extrinsics['t'], dtype=np.float32)
 
+def main(source, size, flip, fps, skip_first_frames):
 
-# web camera
-# video_player = VideoPlayer(0, flip=True, fps=30, skip_first_frames=0)
+    first_time = True
 
-# youtube video
-url = "youtube video url" 
+    if source.startswith("https"):
+        video=pafy.new(source)
+        best = video.getbest(preftype="mp4")
+        source = best.url
+    elif source == "0":
+        source = int(source)
 
-video = pafy.new(url)
-best = video.getbest(preftype="mp4")
-video_player = VideoPlayer(best.url, flip=False, fps=30, skip_first_frames=60)
+    # web camera
+    video_player = VideoPlayer(source, size, flip, fps, skip_first_frames)
 
-video_player.start()
+    video_player.start()
 
-while cv2.waitKey(1) != 27:
-    current_time = cv2.getTickCount()
-    frame = video_player.next()
-    
-    if frame is None:
-        break
-
-    if threading.active_count() == 2:
-        th = MultiThread(frame, model, stride, fx, R, t, infer_request,
-                input_tensor_name,  plotter, canvas_3d, canvas_3d_window_name, current_time, mean_time)
-
-        if first_time:
-            frame, before_3d_frame = th.run()
-            first_time = False
-        else:
-            frame, before_3d_frame = th.run(before_3d_frame)
+    while cv2.waitKey(1) != 27:
+        current_time = cv2.getTickCount()
+        frame = video_player.next()
         
+        if frame is None:
+            break
 
-    # 3d Human pose
-    cv2.imshow(canvas_3d_window_name, canvas_3d)
+        if threading.active_count() == 2:
+            th = MultiThread(frame, model, stride, fx, R, t, infer_request,
+                    input_tensor_name,  plotter, canvas_3d, canvas_3d_window_name, current_time, mean_time)
 
-    # 2d Human pose
-    cv2.imshow("ICV 3D Human Pose Estimation", frame)
+            if first_time:
+                frame, before_3d_frame = th.run()
+                first_time = False
+            else:
+                frame, before_3d_frame = th.run(before_3d_frame)
+            
 
-video_player.stop()
-cv2.destroyAllWindows()
+        # 3d Human pose
+        cv2.imshow(canvas_3d_window_name, canvas_3d)
+
+        # 2d Human pose
+        cv2.imshow("ICV 3D Human Pose Estimation", frame)
+
+    video_player.stop()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source', type=str, default="0", help="string. default: your webcam \n video path: local video \n video URL: youtube video")
+    parser.add_argument('--size', type=int, default=None, help="Image size")
+    parser.add_argument('--flip', type=bool, default=False, help="bool. Flipped images")
+    parser.add_argument('--fps', type=int, default=30, help="int. Change frame rate")
+    parser.add_argument('--skip_first_frames', type=int, default=0, help="int. Skip frames when load video or video URL")
+    FLAGS, unparsed = parser.parse_known_args()
+    main(FLAGS.source, FLAGS.size, FLAGS.flip, FLAGS.fps, FLAGS.skip_first_frames)
